03ae0ca8b796ea7b84ba89893c22c5f8f38eafaf
Михаил Шмыков <m7shmykov@gmail.com>
2020-07-03 10:05:22 +0300

    solve cod 404 & 400 problems commit

--- src/App.js
+++ src/App.js
@@ -1,63 +1,112 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import Loader from './Loader.jsx';
-import Time from './Time.jsx';
+
 
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-  const [icon, setIcon] = useState('');
-  const [response, setResponse] = useState('');
-  const updateResponse = (event) => {
+const [weatherData, setWeatherData] = useState(null);
+const [response, setResponse] = useState('');
+const updateResponse = (event) => {
     setResponse(event.target.value)
-};
-  
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(function showPosition(position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
+}; // input update
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        setWeatherData(data);
-        setIcon(`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
-    })
-})}, []); // get current weather
-
-const answerCheck = (event) => {
-  if (event.key === "Enter") {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${response}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
-      
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(function showPosition(position) {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`; 
+  // Not using https://www.metaweather.com/api/ because access to fetch has been blocked by CORS policy. Besides 'openweathermap' has better geolocation options and more locations to find.
 
   fetch(url)
-  .then(response => response.json())
-  .then(data => {
-      setWeatherData(data);
-      setIcon(`http://openweathermap.org/img/w/${data.weather[0].icon}.png`);
-  })
+  .then(res => res.json())
+  .then(data => setWeatherData(data))
+})}, []); // get current weather
+
+const returnToStart = () => {
+  setWeatherData(
+    () => {
+      navigator.geolocation.getCurrentPosition(function showPosition(position) {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
+  
+      fetch(url)
+      .then(res => res.json())
+      .then(data => setWeatherData(data))
+  })}
+  )
+};// get back to start
+
+const weatherCheck = (event) => {
+  if (event.key === 'Enter') {
+    setTimeout(() => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${response}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
+  
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setWeatherData(data))
+      }, 1000); // Set timeout for better UI
+      
 }};// get select weather
 
-  
+if (!weatherData ) {
+  return (
+  <div className='root'>
+     <div className='container'>
+         <Loader />
+      </div>
+    </div>
+  )
+}; // Render when we haven't any data from server
+if (weatherData.cod === '404') {
+  return (
+      <div className='root'>
+        <div className='container'>
+             <p><b>
+             Населённый пункт не найдет!
+             </b></p>
+             Возможна ошибка при вводе.<br></br>
+             Вернитесь к началу и повторите попытку.
+                 <button id='button' onClick={returnToStart}>
+                  Вернуться к началу
+                  </button>
+         </div>
+      </div>
+  )
+}; // Render when we get cod 404
+if (weatherData.cod === '400') {
+  return (
+      <div className='root'>
+        <div className='container'>
+             <p><b>
+             Вы ничего не ввели!
+             </b></p>
+             Вернитесь к началу и введите населённый пункт для поиска.
+                 <button id='button' onClick={returnToStart}>
+                  Вернуться к началу
+                  </button>
+         </div>
+      </div>
+  )
+}; // Render when we get cod 400
   return (
   <div className='root'>
     <div className='container'>
-      {!weatherData ? <Loader /> :
-      (
       <>
         <div className='header'>
-          <input id="input" onKeyPress={answerCheck} onChange={updateResponse} defaultValue={weatherData.name}  />
-          <img src={icon} alt={weatherData.description} />
+          <input type='text' id='input' onKeyPress={weatherCheck} onChange={updateResponse} defaultValue={weatherData.name}  /> 
         </div>
-          <p>Текущая температура {Math.round(weatherData.main.temp)} C°</p>
-          <p>Скорость ветра {Math.round(weatherData.wind.speed)} м/с</p>
-          <p>Влажность {weatherData.main.humidity} %</p>
-        <Time />
-    </>
-    )}
+              <div className='description'>
+                    Сейчас {weatherData.weather[0].description}
+                    <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt={'иконка погоды'} />
+              </div>
+                    <p>Текущая температура {Math.round(weatherData.main.temp)} C°</p>
+                    <p>Скорость ветра {Math.round(weatherData.wind.speed)} м/с</p>
+                    <p>Влажность {weatherData.main.humidity} %</p>
+       </>
     </div>
   </div>
-  )
+  ); // Initial render
 }
 
 export default App;
